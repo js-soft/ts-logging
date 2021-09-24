794ca7c6f5d9f6f77994bf59248eee4090a05d6e
Timo Notheisen <timo.notheisen@js-soft.com>
2021-09-24 12:32:17 +0200

    fix: remove compiled files

--- packages/abstractions/src/ILogger.d.ts
+++
@@ -1,8 +0,0 @@
-export interface ILogger {
-    trace(...messages: any[]): void;
-    debug(...messages: any[]): void;
-    info(...messages: any[]): void;
-    warn(...messages: any[]): void;
-    error(...messages: any[]): void;
-    fatal(...messages: any[]): void;
-}
--- packages/abstractions/src/ILoggerFactory.d.ts
+++
@@ -1,4 +0,0 @@
-import { ILogger } from "./ILogger";
-export interface ILoggerFactory {
-    getLogger(name: string | Function): ILogger;
-}
