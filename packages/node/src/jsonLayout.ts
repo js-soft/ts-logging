ab67c8d3038ee6f8d527bb3b4d23221600e27029
Sebastian Mahr <sebbi08@users.noreply.github.com>
2024-08-01 16:24:53 +0200

    Feature/Add correlation id to json output (#10)
    * feat: add correlation id to json output
    
    * chore: update package-lock
    
    * fix: always
    
    * fix: correct version bumps
    
    * chore: patch bumped package
    
    * Update packages/node/src/jsonLayout.ts
    
    Co-authored-by: Timo Notheisen <65653426+tnotheis@users.noreply.github.com>
    
    * chore: apply PR comment
    
    * chore: apply PR comment
    
    * chore: rebuild package lock
    
    ---------
    
    Co-authored-by: Julian König <julian.koenig@js-soft.com>
    Co-authored-by: Timo Notheisen <65653426+tnotheis@users.noreply.github.com>

--- packages/node/src/jsonLayout.ts
+++ packages/node/src/jsonLayout.ts
@@ -1,15 +1,19 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 
+import correlationIdLib from "correlation-id";
 import stringify from "json-stringify-safe";
 import log4js, { LoggingEvent } from "log4js";
 import util from "util";
 
 function jsonFormat(logEvent: LoggingEvent) {
-    const json = {
+    const json: any = {
         "@t": logEvent.startTime,
         "@mt": util.format(...logEvent.data),
         "@l": logEvent.level.levelStr
     };
+
+    const correlationId = correlationIdLib.getId();
+    if (correlationId) json["correlationId"] = correlationId;
 
     return stringify(json);
 }
